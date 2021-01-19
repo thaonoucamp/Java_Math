92fe28f9cf974f152771d7c610d5a60d3ab69254
thaonoucamp <dangxuanthaoht@gamil.com>
2021-01-19 22:17:44 +0700

    add

---
+++ src/math/Toan.java
@@ -0,0 +1,14 @@
+package math;
+
+public class Toan {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(4.6)); // Ham tinh can bac 2;
+        System.out.println(Math.PI); // Ham goi PI;
+        System.out.println(Math.abs(-189)); // Ham tinh gia tri tuyet doi;
+        System.out.println(Math.random()); // Ham in ra gia tri tu dong;
+        System.out.println(Math.ceil(7.8)); // Ham lam tron len;
+        System.out.println(Math.floor(7.8)); // Ham lam tron xuong;
+
+
+    }
+}
